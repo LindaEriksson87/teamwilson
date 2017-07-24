80256d44b87257140062c06a86772d4473b2f232
Emilee <emileelam@hotmail.com>
2017-07-24 10:16:41 +0100

    Add files via upload

---
+++ SkyWilson/view/view.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Shop\View;
+
+const TEMPLATE_EXTENSION = '.phtml';
+const TEMPLATE_FOLDER = 'templates/';
+const TEMPLATE_PREFIX = 'shop_view_';
+
+function display($template, $variables = [], $extension = TEMPLATE_EXTENSION) {
+    extract ($variables);
+    
+    ob_start();
+    include TEMPLATE_FOLDER . TEMPLATE_PREFIX . $template . TEMPLATE_EXTENSION;
+    return ob_get_clean();
+}
+
